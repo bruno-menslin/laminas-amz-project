76078ddb0bac35e54564f9c0ed73d35106804790
Bruno Menslin <brunomenslin@outlook.com>
2021-03-03 11:35:27 -0300

    add input filters and validators

--- module/Local/src/Model/Local.php
+++ module/Local/src/Model/Local.php
@@ -2,15 +2,82 @@
 
 namespace Local\Model;
 
-class Local {
+use DomainException;
+use Laminas\Filter\ToInt;
+use Laminas\Filter\StripTags;
+use Laminas\Filter\StringTrim;
+use Laminas\Validator\StringLength;
+use Laminas\InputFilter\InputFilter;
+use Laminas\InputFilter\InputFilterInterface;
+use Laminas\InputFilter\InputFilterAwareInterface;
+
+class Local implements InputFilterAwareInterface 
+{
     public $id;
     public $name;
     public $type_id;
+    
+    private $inputFilter;
     
     public function exchangeArray(array $data)
     {
         $this->id = (!empty($data['id'])) ? $data['id'] : null;
         $this->name = (!empty($data['name'])) ? $data['name'] : null;
         $this->type_id = (!empty($data['type_id'])) ? $data['type_id'] : null;
+    }
+    
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(sprintf(
+            '%s does not allow injection of an alternate input filter',
+            __CLASS__                
+        ));
+    }
+    
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+        
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add([
+            'name' => 'id',
+            'required' => true,
+            'filters' => [
+                ['name' => ToInt::class], // so aceita inteiros
+            ],
+        ]);
+
+        $inputFilter->add([
+            'name' => 'name',
+            'required' => true,
+            'filters' => [
+                ['name' => StripTags::class], // remover HTMl indesejado 
+                ['name' => StringTrim::class], // remover espacos em branco desnecessarios
+            ],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 60,
+                    ],
+                ],
+            ],
+        ]);
+        
+        $inputFilter->add([
+            'name' => 'type_id',
+            'required' => true,
+            'filters' => [
+                ['name' => ToInt::class],
+            ],
+        ]);
+
+        $this->inputFilter = $inputFilter;
+        return $this->inputFilter;        
     }
 }
